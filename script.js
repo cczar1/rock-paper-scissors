264b9f1819c9d6ffcb390e2bb734a863ee25b572
cczar87 <179654294+cczar87@users.noreply.github.com>
2025-01-01 13:17:37 -0500

    Create new function for user choice
    Prompt user to enter valid choice
    
    Return user choice if valid
    
    Prompt user if an invalid choice is entered

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-//Rock, Paper, Scissors
+//Rock, Paper, Scissors Game
 
 const getComputerChoice = () => {
 
   const randomNumber = Math.floor(Math.random() * 3);
 
-  if(randomNumber === 0) {
+  if (randomNumber === 0) {
     return 'rock';
-  }else if (randomNumber === 1) {
+  } else if (randomNumber === 1) {
     return 'paper';
-  }else if (randomNumber === 2) {
+  } else if (randomNumber === 2) {
     return 'scissors';
   }
-}; console.log(getComputerChoice());
+
+}; 
+
+const getHumanChoice = () => {
+  
+  let humanChoice = prompt('Please enter rock, paper, or scissors.');
+ 
+  if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+    return humanChoice;
+  } else {
+    prompt('Please enter a valid choice');
+  }
+
+};
