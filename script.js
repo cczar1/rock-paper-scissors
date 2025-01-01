9d2f5c3bd3b929f2c1624c6fbb1703b70dde51b1
cczar87 <179654294+cczar87@users.noreply.github.com>
2025-01-01 12:29:30 -0500

    Create the game Rock, Paper, Scissors
    Create new function for computer choice
    
    Computer choice randomly returns rock, paper, or scissors

---
+++ script.js
@@ -0,0 +1,14 @@
+//Rock, Paper, Scissors
+
+const getComputerChoice = () => {
+
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  if(randomNumber === 0) {
+    return 'rock';
+  }else if (randomNumber === 1) {
+    return 'paper';
+  }else if (randomNumber === 2) {
+    return 'scissors';
+  }
+}; console.log(getComputerChoice());
